4512a33b77ed0f22efb1d4a6b92f181b57776833
Caroline Dallmann <caroline.dallmann@capgemini.com>
2022-04-13 16:28:03 -0300

    criação json server e classe Pessoa

---
+++ classePessoa.js
@@ -0,0 +1,15 @@
+class Pessoa {
+    constructor(nome, dataNasc, cidadeNat, estadoNat, rua, numero, cep, cidadeRes, estadoRes, telefone, email) {
+        this.nome = nome;
+        this.dataNasc = dataNasc;
+        this.cidadeNat = cidadeNat;
+        this.estadoNat = estadoNat;
+        this.rua = rua;
+        this.numero = numero;
+        this.cep = cep;
+        this.cidadeRes = cidadeRes;
+        this.estadoRes = estadoRes;
+        this.telefone = telefone;
+        this.email = email;
+    }
+}
