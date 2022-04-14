66440cbbaa69b58356e9fe41e49132ca7d0dfd5f
Caroline Dallmann <caroline.dallmann@capgemini.com>
2022-04-14 07:35:05 -0300

    validação form e estilização tabela e checkbox

--- utilsPessoa.js
+++ utilsPessoa.js
@@ -12,13 +12,32 @@ let email = document.querySelector('#email')
 
 let corpoTabela = document.querySelector('#corpoTab')
 
+//regras do form
+let regras = [
+    { name: "nome" },
+    { name: "dataNasc" },
+    { name: "naturalCid" },
+    { name: "naturalEst" },
+    { name: "rua" },
+    { name: "numero" },
+    { name: "cep" },
+    { name: "cidadeRes" },
+    { name: "estadoRes" },
+    { name: "telefone" },
+    { name: "email" }
+]
+
 // salva primeiro cadastro
 function salvar() {
     let params = new URLSearchParams(window.location.search)
     let idPessoa = params.get('id')
+
     let pessoa = new Pessoa(nome.value, dataNasc.value, naturalCid.value,
         naturalEst.value, rua.value, numero.value, cep.value,
         cidadeRes.value, estadoRes.value, telefone.value, email.value)
+    if (!validaForm()) {
+        return
+    }
     if (idPessoa) {
         pessoa.id = idPessoa
         pessoa.update()
@@ -38,14 +57,39 @@ function criaTabela(dadosRecebidos) {
     var trs = ""
     for (let indice in listaPessoas) {
         trs += `<tr> 
+                <th scope="row" class="more" onclick="toggle(this)">+</th>
                 <td>${listaPessoas[indice].id} </td>
                 <td>${listaPessoas[indice].nome} </td>
                 <td>${listaPessoas[indice].dataNasc} </td>
                 <td>${listaPessoas[indice].cidadeRes} </td>
-                <td><button type="button" onclick="window.location.href = 'cadastro.html?id=${listaPessoas[indice].id}'" class="btn btn-outline-warning">Editar</button></td>
-                <td><button type="button" onclick="excluir(${listaPessoas[indice].id})" class="btn btn-outline-danger">Excluir</button>
-                </tr>`
-       
+                <td><span class="material-icons-outlined" onclick="window.location.href = 'cadastro.html?id=${listaPessoas[indice].id}'">edit</span> </td>
+                <td><span class="material-icons-outlined" onclick="excluir(${listaPessoas[indice].id})">delete</span></td>
+                </tr>
+                <tr style="display: none;">
+                    <td colspan="4">
+                        <div class="row">
+                        <div class="col">
+                            <div><b>Nome:</b> ${listaPessoas[indice].nome}</div>
+                            <div><b>Nascimento:</b> ${listaPessoas[indice].dataNasc}</div>
+                            <div><b>Naturalidade:</b> ${listaPessoas[indice].cidadeNat}</div>
+                            <div><b>Estado:</b> ${listaPessoas[indice].estadoNat}</div>
+                        </div>
+                        <div class="col">
+                            <div><b>Rua:</b> ${listaPessoas[indice].rua}</div>
+                            <div><b>Número:</b> ${listaPessoas[indice].numero}</div>
+                            <div><b>CEP:</b> ${listaPessoas[indice].cep}</div>
+                            <div><b>Cidade:</b> ${listaPessoas[indice].cidadeRes} </div>
+                        </div>
+                        <div class="col">
+                            <div><b>Estado:</b> ${listaPessoas[indice].estadoRes}</div>
+                            <div><b>Telefone:</b> ${listaPessoas[indice].telefone}</div>
+                            <div><b>Email:</b> ${listaPessoas[indice].email}</div>
+                            <div><b>ID:</b> ${listaPessoas[indice].id}</div>
+                        </div>
+                        </div>
+                    </td>
+                    </tr>`
+
     }
     corpoTabela.innerHTML = trs
 }
@@ -77,4 +121,32 @@ function capturaParametro() {
 function excluir(id) {
     let obj = new Pessoa()
     obj.delete(id)
+}
+
+//função validação com base nas regras
+
+function validaForm() {
+    for (let regra of regras) {
+        let input = document.querySelector(`#${regra.name}`)
+
+        if (input.value === '') {
+            input.focus()
+            alert('Campo obrigatório')
+            return false
+        }
+    }
+    return true
+}
+
+//
+function toggle(th) {
+    let line = th.parentNode.nextElementSibling.style;
+    if (line.display === "none") {
+        th.innerText = "-";
+        line.display = "table-row";
+    } else {
+        th.innerText = "+";
+        line.display = "none";
+    }
+    console.log();
 }
